68bd342432fac5d4756d4ca52ec566d42442f9fa
Javier Parada <javierparada73@outlook.com>
2022-01-15 14:14:31 +0100

    Add basic workflows and tests

--- deepcomparer/deep_compare.py
+++ deepcomparer/deep_compare.py
@@ -3,6 +3,26 @@ from typing import Any
 
 def deep_compare(
         obj1: Any,
-        obj2: Any
+        obj2: Any,
 ) -> bool:
-    pass
+    res = __rec_helper(obj1, obj2)
+    return res
+
+
+def __rec_helper(obj1: Any, obj2: Any, ) -> bool:
+    if type(obj1) != type(obj2):
+        return False
+
+    if isinstance(obj1, dict):
+        pass
+
+    if isinstance(obj1, list):
+        pass
+
+    if isinstance(obj1, set):
+        pass
+
+    if isinstance(obj1, tuple):
+        pass
+
+    return obj1 == obj2
